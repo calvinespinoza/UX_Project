0bf21d2aa8b7a8d8e435eaf31fadfe3a716916cd
calvinespinoza <calvin_espinoza@hotmail.com>
2018-06-17 22:50:17 -0600

    firebase faiñ

--- pijin-hn/src/App.js
+++ pijin-hn/src/App.js
@@ -3,13 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './components/Navbar';
 import { Jumbotron } from './components/Jumbotron';
+import * as firebase from 'firebase';
 
 class App extends Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      user : null,
+    }
+    this.handleLoginGoogle = this.handleLoginGoogle.bind(this);
+  }
+
+  handleLoginGoogle(){
+    if (this.state.user){
+      firebase.auth().signOut()
+        .then(result => console.log(`${result} ha salido`))
+        .catch(err =>  console.log(err))
+      this.setState({user : null, google: "Google"})
+    }else {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithPopup(provider)
+        .then(res => {
+          this.setState({
+            user : res.user
+          })
+          this.setState({google : "Logout Google"})
+        })
+        .catch(err=> console.log("error: "+err))
+        
+    }
+  }
+
   render() {
     return (
       <div>
         <Navbar />
-        <Jumbotron />
+        <Jumbotron event={this.handleLoginGoogle}/>
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
--- pijin-hn/src/components/Jumbotron.js
+++ pijin-hn/src/components/Jumbotron.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
-import './Jumbotron.css'
+import './Jumbotron.css';
+import * as firebase from 'firebase';
 
 export class Jumbotron extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.writingDB = this.writingDB.bind(this);
+    }
+
+    writingDB() {
+        const rootRef = firebase.database().ref();
+
+        rootRef.push().set({
+            name: "HOLA"
+        });
+    }
+
     render() {
         return (
             <div id="jumbotron" class="jumbotron jumbotron-fluid">
@@ -10,7 +25,7 @@ export class Jumbotron extends Component {
                     <p class="lead" id="subTitle">Hazte una cuenta</p>
                 </div>
                 <div id="authButtons">
-                    <button class="w3-button w3-round-xxlarge" id="bt-login">SIGN UP</button>
+                    <button class="w3-button w3-round-xxlarge" onClick={this.props.event} id="bt-login">SIGN UP</button>
                     <button class="w3-button w3-round-xxlarge" id="bt-login">LOG IN</button>
                 </div>
             </div>
