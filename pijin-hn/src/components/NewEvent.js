fdc2829c8a3062f564b34780a838e58003007563
calvinespinoza <calvin_espinoza@hotmail.com>
2018-07-02 08:32:56 -0600

    categories added

--- pijin-hn/src/components/NewEvent.js
+++ pijin-hn/src/components/NewEvent.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import './Home.css';
 import * as routes from '../constants/routes';
+import { withRouter } from 'react-router-dom';
 
-const NewEvent = () =>
+const NewEvent = ({ history }) =>
     <div id="event">
         <div id="main-title">Create Event</div>
-        <NewEventForm />
+        <NewEventForm history={history} />
     </div>
 
 const INITIAL_STATE = {
@@ -16,6 +17,17 @@ const INITIAL_STATE = {
     horainicio: '',
     fechafinal: '',
     horafinal: '',
+
+    fiesta: false,
+    comida: false,
+    envivo: false,
+    gaming: false,
+    musicon: false,
+    bares: false,
+    airelibre: false,
+    deportes: false,
+    arte: false,
+
     error: null,
 }
 
@@ -54,6 +66,19 @@ class NewEventForm extends Component {
                     "Hora Final": this.state.horafinal,
                     "Llave": key,
                     "CreatorId": uid,
+
+                    "Categorias": {
+                        "Fiesta": (this.state.fiesta !== null ? (this.state.fiesta ? true : false) : false),
+                        "Comida": (this.state.comida !== null ? (this.state.comida ? true : false) : false),
+                        "En Vivo":(this.state.envivo !== null ? (this.state.envivo ? true : false) : false),
+                        "Gaming": (this.state.gaming !== null ? (this.state.gaming ? true : false) : false),
+                        "Musicon": (this.state.musicon !== null ? (this.state.musicon ? true : false) : false),
+                        "Bares": (this.state.bares != null ? (this.state.bares ? true : false) : false),
+                        "Aire Libre": (this.state.airelibre !== null ? (this.state.airelibre ? true : false) : false),
+                        "Deportes": (this.state.deportes !== null ? (this.state.deportes ? true : false) : false),
+                        "Arte": (this.state.arte !== null ? (this.state.arte ? true : false) : false),
+                    }
+
                     /*
                     "Host": {
                         "Nombre": hostname,
@@ -78,6 +103,17 @@ class NewEventForm extends Component {
             horainicio,
             fechafinal,
             horafinal,
+
+            fiesta,
+            comida,
+            envivo,
+            gaming,
+            musicon,
+            bares,
+            airelibre,
+            deportes,
+            arte,
+
             error,
         } = this.state;
         const {
@@ -86,7 +122,7 @@ class NewEventForm extends Component {
 
         { this.addEvent() }
 
-
+        //{ history.push(routes.HOME) }
         event.preventDefault();
     }
 
@@ -99,6 +135,17 @@ class NewEventForm extends Component {
             horainicio,
             fechafinal,
             horafinal,
+
+            fiesta,
+            comida,
+            envivo,
+            gaming,
+            musicon,
+            bares,
+            airelibre,
+            deportes,
+            arte,
+
             error,
         } = this.state;
 
@@ -125,7 +172,6 @@ class NewEventForm extends Component {
                     <div id="input-list-item">
                         <input id="new-event-input"
                             className="uk-input"
-
                             value={locationname}
                             onChange={event => this.setState(byPropKey('locationname', event.target.value))}
                             type="text"
@@ -171,15 +217,51 @@ class NewEventForm extends Component {
                     </div>
                     <div id="min-title">Categorias</div>
                     <ul class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
-                        <label class="uk-width-1-3"><input class="uk-checkbox" type="checkbox" /> Fiesta </label>
-                        <label class="uk-width-1-3"><input class="uk-checkbox" type="checkbox" /> Comida </label>
-                        <label class="uk-width-1-3"><input class="uk-checkbox" type="checkbox" /> En Vivo</label>
-                        <label class="uk-width-1-3"><input class="uk-checkbox" type="checkbox" /> Musicon</label>
-                        <label class="uk-width-1-3"><input class="uk-checkbox" type="checkbox" /> Gaming </label>
-                        <label class="uk-width-1-3"><input class="uk-checkbox" type="checkbox" /> Bares  </label>
-                        <label class="uk-width-1-3"><input class="uk-checkbox" type="checkbox" /> Aire Libre</label>
-                        <label class="uk-width-1-3"><input class="uk-checkbox" type="checkbox" /> Deportes</label>
-                        <label class="uk-width-1-3"><input class="uk-checkbox" type="checkbox" /> Arte   </label>
+                        <label class="uk-width-1-3">
+                            <input class="uk-checkbox" name="options" type="checkbox"
+                                onChange={event => this.setState(byPropKey('fiesta', event.target.checked))}
+                            /> Fiesta
+                        </label>
+                        <label class="uk-width-1-3">
+                            <input class="uk-checkbox" name="options" type="checkbox"
+                                onChange={event => this.setState(byPropKey('comida', event.target.checked))}
+                            /> Comida
+                        </label>
+                        <label class="uk-width-1-3">
+                            <input class="uk-checkbox" name="options" type="checkbox"
+                                onChange={event => this.setState(byPropKey('envivo', event.target.checked))}
+                            /> En Vivo
+                        </label>
+                        <label class="uk-width-1-3">
+                            <input class="uk-checkbox" name="options" type="checkbox"
+                                onChange={event => this.setState(byPropKey('musicon', event.target.checked))}
+                            /> Musicon
+                        </label>
+                        <label class="uk-width-1-3">
+                            <input class="uk-checkbox" name="options" type="checkbox"
+                                onChange={event => this.setState(byPropKey('gaming', event.target.checked))}
+                            /> Gaming
+                        </label>
+                        <label class="uk-width-1-3">
+                            <input class="uk-checkbox" name="options" type="checkbox"
+                                onChange={event => this.setState(byPropKey('bares', event.target.checked))}
+                            /> Bares
+                        </label>
+                        <label class="uk-width-1-3">
+                            <input class="uk-checkbox" name="options" type="checkbox"
+                                onChange={event => this.setState(byPropKey('airelibre', event.target.checked))}
+                            /> Aire Libre
+                        </label>
+                        <label class="uk-width-1-3">
+                            <input class="uk-checkbox" name="options" type="checkbox"
+                                onChange={event => this.setState(byPropKey('deportes', event.target.checked))}
+                            /> Deportes
+                        </label>
+                        <label class="uk-width-1-3">
+                            <input class="uk-checkbox" name="options" type="checkbox"
+                                onChange={event => this.setState(byPropKey('arte', event.target.checked))}
+                            /> Arte
+                        </label>
                     </ul>
                 </form>
                 <button id="bt-signup" className="w3-button w3-round-xxlarge" disabled={isInvalid} onClick={this.onClick}>
